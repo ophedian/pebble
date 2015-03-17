f5c8d80c14d8566659cedee25e7c45f9c3c766d2
Abbey Gwayambadde <ophedian@gmail.com>
2015-03-17 13:32:23 -0400

    Initial file check-in

---
+++ app.js
@@ -0,0 +1,34 @@
+var UI = require('ui');
+var weather = require("Weather");
+var path = require("LifePath");
+
+// create the initial menu
+var mainMenu = new UI.Menu({
+		sections : [{
+				items : [{
+						title : "Weather",
+						icon : 'images/menu_icon.png',
+						subtitle : "What To Wear"
+					}, {
+						title : "Lucky Day",
+						subtitle : "Feeling lucky"
+					}
+				]
+			}
+		]
+	});
+
+//Create event handlers for the menu
+mainMenu.on("select", function (e) {
+	console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+	console.log('The item is titled "' + e.item.title + '"');
+
+	switch (e.itemIndex) {
+	case 0:
+		weather.show();
+		break;
+	case 1:
+		path.showMenu();
+		break;
+	}
+});
